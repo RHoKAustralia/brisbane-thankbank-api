b45ab3c871598e3dfec4d46c7be7d10894583ac6
Gabriel Gizotti <hajime.gizotti@gmail.com>
2015-06-14 14:45:50 +1000

    YOOOOLOOOOO

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -3,5 +3,5 @@ class Checkin < ActiveRecord::Base
   belongs_to :coupon
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_attachment_content_type :image, content_type: ['application/octet-stream', 'image/jpeg']
 end

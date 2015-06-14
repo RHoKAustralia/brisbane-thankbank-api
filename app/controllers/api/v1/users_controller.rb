8f2dd4972e4307df18cfcdbec6dfedf013a2fda0
Gabriel Gizotti <hajime.gizotti@gmail.com>
2015-06-14 11:18:09 +1000

    noobs will be noobs

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -21,7 +21,7 @@ class Api::V1::UsersController < Api::BaseController
     new_params = params.require(:user).permit(:email, :facebook_uid, :first_name, :last_name)
 
     if params[:user].present? && params[:user][:avatar_url].present?
-      new_params[:avatar] = open(params[:avatar_url])
+      new_params[:avatar] = open(params[:user][:avatar_url])
     end
 
     new_params
